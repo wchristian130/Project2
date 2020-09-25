7e742bba0904812484e956a05040b9fefab56a43
Will Christian <william.christian.19@cnu.edu>
2020-09-25 10:00:22 -0400

    Added missing fileio directory

---
+++ fileio/fileio.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include "../includes/fileio.h"
+#include "../includes/constants.h"
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, std::vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, std::vector<process> &myProcesses){
+	return UNIMPLEMENTED;
+}
